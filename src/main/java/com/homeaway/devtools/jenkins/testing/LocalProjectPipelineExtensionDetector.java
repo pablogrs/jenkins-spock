0f352134c8f37a2c1d00ccf465a19c94f12f0f47
pablosan <pablo.sanchez@aerlingus.com>
2023-09-13 12:33:51 +0100

    Prepare to use with Java 11

--- src/main/java/com/homeaway/devtools/jenkins/testing/LocalProjectPipelineExtensionDetector.java
+++ src/main/java/com/homeaway/devtools/jenkins/testing/LocalProjectPipelineExtensionDetector.java
@@ -86,7 +86,7 @@ public class LocalProjectPipelineExtensionDetector extends APipelineExtensionDet
 
 			try {
 				clazz = Class.forName( classname );
-			} catch( ClassNotFoundException e ) {
+			} catch( NoClassDefFoundError e ) {
 				failures.put( classname, e );
 				continue;
 			}
